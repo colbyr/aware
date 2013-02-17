972c731b8d7458f680dcd50b1476d8151e171ac3
Colby Rabideau <idontknow5691@gmail.com>
2013-02-17 14:59:50 -0500

    fixed composer/phpunit

--- tests/AwareModelTest.php
+++ tests/AwareModelTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use \Mockery as m;
+use Awareness\Aware;
 
 class AwareModelTest extends PHPUnit_Framework_TestCase {
 
   function testErrorsMethod() {
-    $model = m::mock('alias:Awareness\Aware');
+    $model = m::mock('alias:Model');
+    $real_model = new Model();
   }
 
 }
