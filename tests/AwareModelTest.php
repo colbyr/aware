f80bc5795a380e49817b0a9ed210406ac37ca11b
Colby Rabideau <idontknow5691@gmail.com>
2013-02-17 12:50:23 -0500

    more restructurez

--- tests/AwareModelTest.php
+++ tests/AwareModelTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class TestTest extends PHPUnit_Framework_TestCase {
+class AwareModelTest extends PHPUnit_Framework_TestCase {
 
   function testBoolean() {
     $this->assertEquals(true, true);
